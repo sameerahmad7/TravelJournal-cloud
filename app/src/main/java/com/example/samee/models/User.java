86a38f5b7c33dc93f429f9502579464ecd7cce58
Sameer Ahmad <sameer_ahmad324@hotmail.com>
2018-10-20 21:07:18 +0500

    Navigation Drawer and Diary Activites added

---
+++ app/src/main/java/com/example/samee/models/User.java
@@ -0,0 +1,64 @@
+package com.example.samee.models;
+
+public class User {
+    public User(String name, String city, String dob, String country, String number) {
+        this.name = name;
+        this.city = city;
+        this.dob = dob;
+        this.country = country;
+        this.number=number;
+    }
+
+
+    String name;
+    String city;
+    String dob;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    String country;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    String number;
+
+}
---
+++ app/src/main/java/com/example/samee/traveljournal/OutOfStateActivity.java
@@ -0,0 +1,110 @@
+package com.example.samee.traveljournal;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class OutOfStateActivity extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+    FirebaseAuth mAuth;
+    ImageView profilePic;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_out_of_state);
+        mAuth=FirebaseAuth.getInstance();
+        mAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()==null)
+                {
+                    startActivity(new Intent(OutOfStateActivity.this,MainActivity.class));
+                    finish();
+                }
+            }
+        });
+
+        dl = (DrawerLayout)findViewById(R.id.activity_main);
+
+        t = new ActionBarDrawerToggle(
+                this, dl, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id==R.id.logout)
+                {
+                    if(null!=mAuth.getCurrentUser()) {
+                        mAuth.signOut();
+                        if(null!= Profile.getCurrentProfile())
+                        {
+                            LoginManager.getInstance().logOut();
+                        }
+
+                    }
+                }
+                else if(id==R.id.profile)
+                {
+
+                    startActivity(new Intent(OutOfStateActivity.this,ProfileActivity.class));
+
+
+                }
+                else if(id==R.id.cityTrips)
+                {
+                    startActivity(new Intent(OutOfStateActivity.this,CityTripsActivity.class));
+
+                }
+                else if(id==R.id.outCityTrips)
+                {
+                    startActivity(new Intent(OutOfStateActivity.this,OutOfCityActivity.class));
+
+                }
+                else if(id==R.id.stateTrips)
+                {
+                    dl.closeDrawers();
+
+                }
+                return true;
+
+
+
+
+            }
+        });
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (t.onOptionsItemSelected(item)) {
+            profilePic=(ImageView)findViewById(R.id.profilePic);
+            Glide.with(this)
+                    .load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString())
+                    .into(profilePic);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
